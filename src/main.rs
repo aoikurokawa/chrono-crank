5025d4060ddf5d6b37f4e9bb598b4a2581aa3a8a
aoikurokawa <ht.so.ik0102@gmail.com>
2023-11-05 20:44:20 +0900

    feat: init command.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{
     collections::HashSet,
     error,
+    path::PathBuf,
     rc::Rc,
     sync::atomic::{AtomicBool, AtomicU64},
     sync::{atomic::Ordering, Arc},
@@ -9,7 +10,7 @@ use std::{
 };
 
 use bip39::{Mnemonic, MnemonicType, Seed};
-use clap::{crate_description, crate_name, value_parser, Arg, ArgMatches, Parser, Subcommand};
+use clap::{crate_description, crate_name, value_parser, Arg, ArgMatches, Parser, Subcommand, Command};
 use solana_clap_v3_utils::{
     input_parsers::STDOUT_OUTFILE_TOKEN,
     input_validators::is_prompt_signer_source,
@@ -41,37 +42,7 @@ use solana_sdk::{
     signer::Signer,
 };
 
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Cli {
-    #[command(subcommand)]
-    command: Command,
-}
-
-#[derive(Debug, Subcommand)]
-enum Command {
-    Pubkey {},
-    New {
-        #[arg(short, long, value_name = "FILEPATH", help = "Path to generated file")]
-        outfile: String,
-
-        #[arg(short, long, help = "Path to generated file")]
-        force: String,
-
-        #[arg(short, long, help = "Do not display seed phrase. Useful when piping output to other programs that prompt for user input, like gpg")]
-        silent: String,
-    },
-    Recover {},
-    Grind {},
-    Verify {},
-}
-
 fn main() -> Result<(), Box<dyn error::Error>> {
-    let cli = Cli::parse();
-
-    match cli.command {
-        Command::New {} => {}
-    }
     let default_num_threads = num_cpus::get().to_string();
     let matches = app(&default_num_threads, solana_version::version!())
         .try_get_matches()
@@ -584,204 +555,204 @@ fn grind_parse_args(
     grind_matches
 }
 
-// fn app<'a>(num_threads: &'a str, crate_version: &'a str) {
-//     Command::new(crate_name!())
-//         .about(crate_description!())
-//         .version(crate_version)
-//         .subcommand_required(true)
-//         .arg_required_else_help(true)
-//         .arg({
-//             let arg = Arg::new("config_file")
-//                 .short('C')
-//                 .long("config")
-//                 .value_name("FILEPATH")
-//                 .takes_value(true)
-//                 .global(true)
-//                 .help("Configuration file to use");
-//
-//             if let Some(config_file) = solana_cli_config::CONFIG_FILE.as_ref() {
-//                 arg.default_value(&config_file)
-//             }  else {
-//                 arg
-//             }
-//
-//         })
-//         .subcommand(
-//             Command::new("verify")
-//                 .about("Verify a keypair can sign and verify a message.")
-//                 .arg(
-//                     Arg::new("pubkey")
-//                         .index(1)
-//                         .value_name("PUBKEY")
-//                         .takes_value(true)
-//                         .required(true)
-//                         .help("Public key"),
-//                 )
-//                 .arg(
-//                     Arg::new("keypair")
-//                         .index(2)
-//                         .value_name("KEYPAIR")
-//                         .takes_value(true)
-//                         .help("Filepath or URL to a keypair"),
-//                 )
-//         )
-//         .subcommand(
-//             Command::new("new")
-//                 .about("Generate new keypair file from a random seed phrase and optional BIP39 passphrase")
-//                 .disable_version_flag(true)
-//                 .arg(
-//                     Arg::new("outfile")
-//                         .short('o')
-//                         .long("outfile")
-//                         .value_name("FILEPATH")
-//                         .takes_value(true)
-//                         .help("Path to generated file"),
-//                 )
-//                 .arg(
-//                     Arg::new("force")
-//                         .short('f')
-//                         .long("force")
-//                         .help("Overwrite the output file if it exists"),
-//                 )
-//                 .arg(
-//                     Arg::new("silent")
-//                         .short('s')
-//                         .long("silent")
-//                         .help("Do not display seed phrase. Useful when piping output to other programs that prompt for user input, like gpg")
-//                 )
-//                 .arg(
-//                     derivation_path_arg()
-//                 )
-//                 .key_generation_common_args()
-//                 .arg(no_outfile_arg().conflicts_with_all(&["outfile", "silent"]))
-//         )
-//         .subcommand(
-//             Command::new("grind")
-//                 .about("Grind for vanity keypairs")
-//                 .disable_version_flag(true)
-//                 .arg(
-//                     Arg::new("ignore_case")
-//                         .long("ignore-case")
-//                         .help("Performs case insensitive matches"),
-//                 )
-//                 .arg(
-//                     Arg::new("starts_with")
-//                         .long("starts-with")
-//                         .value_name("PREFIX:COUNT")
-//                         .number_of_values(1)
-//                         .takes_value(true)
-//                         .multiple_occurrences(true)
-//                         .multiple_values(true)
-//                         .validator(grind_validator_starts_with)
-//                         .help("Saves specified number of keypairs whos public key starts with the indicated prefix\nExample: --starts-with sol:4\nPREFIX type is Base58\nCOUNT type is u64")
-//                 )
-//                 .arg(
-//                     Arg::new("ends_with")
-//                         .long("ends-with")
-//                         .value_name("SUFFIX:COUNT")
-//                         .number_of_values(1)
-//                         .takes_value(true)
-//                         .multiple_occurrences(true)
-//                         .multiple_values(true)
-//                         .validator(grind_validator_ends_with)
-//                         .help("Saves specified number of keypairs whos public key ends with the indicated suffix\nExample: --ends-with ana:4\nSUFFIX type is Base58\nCOUNT type is u64")
-//                 )
-//                 .arg(
-//                     Arg::new("starts_and_ends_with")
-//                         .long("starts-and-ends-with")
-//                         .value_name("PREFIX:SUFFIX:COUNT")
-//                         .number_of_values(1)
-//                         .takes_value(true)
-//                         .multiple_occurrences(true)
-//                         .multiple_values(true)
-//                         .validator(grind_validator_starts_and_end_with)
-//                         .help("Saves specified number of keypairs whos public key starts and ends with the indicated perfix and suffix\nExample: --starts-and-ends-with sol:ana:4\nPREFIX and SUFFIX type is Base58\nCOUNT type is u64")
-//                 )
-//                 .arg(
-//                     Arg::new("num_threads")
-//                         .long("num-threads")
-//                         .value_name("NUMBER")
-//                         .takes_value(true)
-//                         .value_parser(value_parser!(usize))
-//                         .default_value(num_threads)
-//                         .help("Specify the number of grind threds")
-//                 )
-//                 .arg(
-//                     Arg::new("use_mnemonic")
-//                         .long("use-mnemonic")
-//                         .help("Generate usign a mnemonic key phrase. Expect a siginificant slowdown in this mode")
-//                 )
-//                 .arg(
-//                     derivation_path_arg()
-//                         .requires("use_mnemonic")
-//                 )
-//                 .key_generation_common_args()
-//                 .arg(
-//                     no_outfile_arg()
-//                         .requires("use_mnemonic")
-//                 )
-//         )
-//         .subcommand(
-//             Command::new("pubkey")
-//                 .about("Display the pubkey from a keypair file")
-//                 .disable_version_flag(true)
-//                 .arg(
-//                     Arg::new("keypair")
-//                         .index(1)
-//                         .value_name("KEYPAIR")
-//                         .takes_value(true)
-//                         .help("Filepath or URL to a keypair")
-//                 )
-//                 .arg(
-//                     Arg::new(SKIP_SEED_PHRASE_VALIDATION_ARG.name)
-//                         .long(SKIP_SEED_PHRASE_VALIDATION_ARG.long)
-//                         .help(SKIP_SEED_PHRASE_VALIDATION_ARG.help)
-//                 )
-//                 .arg(
-//                     Arg::new("outfile")
-//                         .short('o')
-//                         .long("outfile")
-//                         .value_name("FILEPATH")
-//                         .takes_value(true)
-//                         .help("Path to generated file")
-//                 )
-//                 .arg(
-//                     Arg::new("force")
-//                         .short('f')
-//                         .long("force")
-//                         .help("Overwrite the output file if it exists")
-//                 )
-//         )
-//         .subcommand(
-//             Command::new("recover")
-//            .about("Recover keypair from seed phrase and optional BIP39 passphrase")
-//                 .disable_version_flag(true)
-//                 .arg(
-//                     Arg::new("prompt_signer")
-//                         .index(1)
-//                         .value_name("KEYPAIR")
-//                         .takes_value(true)
-//                         .validator(is_prompt_signer_source)
-//                         .help("`prompt:` URI schme or `ASK` keyword")
-//                 )
-//                 .arg(
-//                     Arg::new("outfile")
-//                         .short('o')
-//                         .long("outfile")
-//                         .value_name("FILEPATH")
-//                         .takes_value(true)
-//                         .help("Path to generated file")
-//                 )
-//                 .arg(
-//                     Arg::new("force")
-//                         .short('f')
-//                         .long("force")
-//                         .help("Overwrite the output file if it exists")
-//                 )
-//                 .arg(
-//                     Arg::new(SKIP_SEED_PHRASE_VALIDATION_ARG.name)
-//                         .long(SKIP_SEED_PHRASE_VALIDATION_ARG.long)
-//                         .help(SKIP_SEED_PHRASE_VALIDATION_ARG.help)
-//                 )
-//         )
-// }
+fn app<'a>(num_threads: &'a str, crate_version: &'a str) -> Command<'a> {
+    Command::new(crate_name!())
+         .about(crate_description!())
+         .version(crate_version)
+         .subcommand_required(true)
+         .arg_required_else_help(true)
+         .arg({
+             let arg = Arg::new("config_file")
+                 .short('C')
+                 .long("config")
+                 .value_name("FILEPATH")
+                 .takes_value(true)
+                 .global(true)
+                 .help("Configuration file to use");
+
+             if let Some(config_file) = solana_cli_config::CONFIG_FILE.as_ref() {
+                 arg.default_value(&config_file)
+             }  else {
+                 arg
+             }
+
+         })
+         .subcommand(
+             Command::new("verify")
+                 .about("Verify a keypair can sign and verify a message.")
+                 .arg(
+                     Arg::new("pubkey")
+                         .index(1)
+                         .value_name("PUBKEY")
+                         .takes_value(true)
+                         .required(true)
+                         .help("Public key"),
+                 )
+                 .arg(
+                     Arg::new("keypair")
+                         .index(2)
+                         .value_name("KEYPAIR")
+                         .takes_value(true)
+                         .help("Filepath or URL to a keypair"),
+                 )
+         )
+         .subcommand(
+             Command::new("new")
+                 .about("Generate new keypair file from a random seed phrase and optional BIP39 passphrase")
+                 .disable_version_flag(true)
+                 .arg(
+                     Arg::new("outfile")
+                         .short('o')
+                         .long("outfile")
+                         .value_name("FILEPATH")
+                         .takes_value(true)
+                         .help("Path to generated file"),
+                 )
+                 .arg(
+                     Arg::new("force")
+                         .short('f')
+                         .long("force")
+                         .help("Overwrite the output file if it exists"),
+                 )
+                 .arg(
+                     Arg::new("silent")
+                         .short('s')
+                         .long("silent")
+                         .help("Do not display seed phrase. Useful when piping output to other programs that prompt for user input, like gpg")
+                 )
+                 .arg(
+                     derivation_path_arg()
+                 )
+                 .key_generation_common_args()
+                 .arg(no_outfile_arg().conflicts_with_all(&["outfile", "silent"]))
+         )
+         .subcommand(
+             Command::new("grind")
+                 .about("Grind for vanity keypairs")
+                 .disable_version_flag(true)
+                 .arg(
+                     Arg::new("ignore_case")
+                         .long("ignore-case")
+                         .help("Performs case insensitive matches"),
+                 )
+                 .arg(
+                     Arg::new("starts_with")
+                         .long("starts-with")
+                         .value_name("PREFIX:COUNT")
+                         .number_of_values(1)
+                         .takes_value(true)
+                         .multiple_occurrences(true)
+                         .multiple_values(true)
+                         .validator(grind_validator_starts_with)
+                         .help("Saves specified number of keypairs whos public key starts with the indicated prefix\nExample: --starts-with sol:4\nPREFIX type is Base58\nCOUNT type is u64")
+                 )
+                 .arg(
+                     Arg::new("ends_with")
+                         .long("ends-with")
+                         .value_name("SUFFIX:COUNT")
+                         .number_of_values(1)
+                         .takes_value(true)
+                         .multiple_occurrences(true)
+                         .multiple_values(true)
+                         .validator(grind_validator_ends_with)
+                         .help("Saves specified number of keypairs whos public key ends with the indicated suffix\nExample: --ends-with ana:4\nSUFFIX type is Base58\nCOUNT type is u64")
+                 )
+                 .arg(
+                     Arg::new("starts_and_ends_with")
+                         .long("starts-and-ends-with")
+                         .value_name("PREFIX:SUFFIX:COUNT")
+                         .number_of_values(1)
+                         .takes_value(true)
+                         .multiple_occurrences(true)
+                         .multiple_values(true)
+                         .validator(grind_validator_starts_and_end_with)
+                         .help("Saves specified number of keypairs whos public key starts and ends with the indicated perfix and suffix\nExample: --starts-and-ends-with sol:ana:4\nPREFIX and SUFFIX type is Base58\nCOUNT type is u64")
+                 )
+                 .arg(
+                     Arg::new("num_threads")
+                         .long("num-threads")
+                         .value_name("NUMBER")
+                         .takes_value(true)
+                         .value_parser(value_parser!(usize))
+                         .default_value(num_threads)
+                         .help("Specify the number of grind threds")
+                 )
+                 .arg(
+                     Arg::new("use_mnemonic")
+                         .long("use-mnemonic")
+                         .help("Generate usign a mnemonic key phrase. Expect a siginificant slowdown in this mode")
+                 )
+                 .arg(
+                     derivation_path_arg()
+                         .requires("use_mnemonic")
+                 )
+                 .key_generation_common_args()
+                 .arg(
+                     no_outfile_arg()
+                         .requires("use_mnemonic")
+                 )
+         )
+         .subcommand(
+             Command::new("pubkey")
+                 .about("Display the pubkey from a keypair file")
+                 .disable_version_flag(true)
+                 .arg(
+                     Arg::new("keypair")
+                         .index(1)
+                         .value_name("KEYPAIR")
+                         .takes_value(true)
+                         .help("Filepath or URL to a keypair")
+                 )
+                 .arg(
+                     Arg::new(SKIP_SEED_PHRASE_VALIDATION_ARG.name)
+                         .long(SKIP_SEED_PHRASE_VALIDATION_ARG.long)
+                         .help(SKIP_SEED_PHRASE_VALIDATION_ARG.help)
+                 )
+                 .arg(
+                     Arg::new("outfile")
+                         .short('o')
+                         .long("outfile")
+                         .value_name("FILEPATH")
+                         .takes_value(true)
+                         .help("Path to generated file")
+                 )
+                 .arg(
+                     Arg::new("force")
+                         .short('f')
+                         .long("force")
+                         .help("Overwrite the output file if it exists")
+                 )
+         )
+         .subcommand(
+             Command::new("recover")
+            .about("Recover keypair from seed phrase and optional BIP39 passphrase")
+                 .disable_version_flag(true)
+                 .arg(
+                     Arg::new("prompt_signer")
+                         .index(1)
+                         .value_name("KEYPAIR")
+                         .takes_value(true)
+                         .validator(is_prompt_signer_source)
+                         .help("`prompt:` URI schme or `ASK` keyword")
+                 )
+                 .arg(
+                     Arg::new("outfile")
+                         .short('o')
+                         .long("outfile")
+                         .value_name("FILEPATH")
+                         .takes_value(true)
+                         .help("Path to generated file")
+                 )
+                 .arg(
+                     Arg::new("force")
+                         .short('f')
+                         .long("force")
+                         .help("Overwrite the output file if it exists")
+                 )
+                 .arg(
+                     Arg::new(SKIP_SEED_PHRASE_VALIDATION_ARG.name)
+                         .long(SKIP_SEED_PHRASE_VALIDATION_ARG.long)
+                         .help(SKIP_SEED_PHRASE_VALIDATION_ARG.help)
+                 )
+         )
+}
