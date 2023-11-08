6e7708754be3e44562dfba981648264996762f41
aoikurokawa <ht.so.ik0102@gmail.com>
2023-11-09 08:31:55 +0900

    chore: remove comments and remove unused dependencies

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ use keygen::{
 };
 use solana_clap_v3_utils::{
     input_parsers::STDOUT_OUTFILE_TOKEN,
-    keypair::{keypair_from_seed_phrase, prompt_passphrase, SignerFromPathConfig},
+    keypair::{keypair_from_seed_phrase, prompt_passphrase},
 };
 use solana_cli_config::Config;
 use solana_sdk::{
@@ -36,7 +36,6 @@ use solana_sdk::{
 fn main() -> Result<(), Box<dyn error::Error>> {
     let cli = Cli::parse();
     let default_num_threads = num_cpus::get();
-    // let mut wallet_manager = None;
     let config = if let Some(config_file) = &cli.config_file {
         Config::load(config_file.to_str().unwrap()).unwrap_or_default()
     } else {
@@ -60,11 +59,10 @@ fn main() -> Result<(), Box<dyn error::Error>> {
                 None
             } else {
                 path.extend([".config", "solana", "id.json"]);
-                Some(&path)
+                Some(path)
             };
 
             match outfile {
-                // Some(PathBuf::from(STDOUT_OUTFILE_TOKEN)) => (),
                 Some(ref outfile) => {
                     if !force && outfile.exists() {
                         let err_msg = format!(
@@ -87,7 +85,7 @@ fn main() -> Result<(), Box<dyn error::Error>> {
             // TODO: Only accept Engilish for now
             let mnemonic = Mnemonic::new(mnemonic_type, bip39::Language::English);
 
-            let (passphrase, passphrase_message) = if *no_bip39_passphrase {
+            let (passphrase, passphrase_message) = if no_bip39_passphrase {
                 ("".to_string(), "".to_string())
             } else {
                 match prompt_passphrase(
@@ -151,7 +149,7 @@ fn main() -> Result<(), Box<dyn error::Error>> {
             let starts_with_args: HashSet<String> = if let Some(starts_with) = starts_with {
                 starts_with
                     .into_iter()
-                    .map(|s| if *ignore_case { s.to_lowercase() } else { s.clone() })
+                    .map(|s| if ignore_case { s.to_lowercase() } else { s.clone() })
                     .collect()
             } else {
                 HashSet::new()
@@ -159,7 +157,7 @@ fn main() -> Result<(), Box<dyn error::Error>> {
             let ends_with_args: HashSet<String> = if let Some(ends_with) = ends_with {
                 ends_with
                     .into_iter()
-                    .map(|s| if *ignore_case { s.to_lowercase() } else { s.clone() })
+                    .map(|s| if ignore_case { s.to_lowercase() } else { s.clone() })
                     .collect()
             } else {
                 HashSet::new()
@@ -168,7 +166,7 @@ fn main() -> Result<(), Box<dyn error::Error>> {
                 if let Some(starts_and_ends_with) = starts_and_ends_with {
                     starts_and_ends_with
                         .into_iter()
-                        .map(|s| if *ignore_case { s.to_lowercase() } else { s.clone() })
+                        .map(|s| if ignore_case { s.to_lowercase() } else { s.clone() })
                         .collect()
                 } else {
                     HashSet::new()
@@ -182,25 +180,23 @@ fn main() -> Result<(), Box<dyn error::Error>> {
             }
 
             let num_threads = match num_threads {
-                Some(n) => *n,
+                Some(n) => n,
                 None => default_num_threads,
             };
 
             let grind_matches = grind_parse_args(
-                *ignore_case,
+                ignore_case,
                 starts_with_args,
                 ends_with_args,
                 starts_and_ends_with_args,
                 num_threads,
             );
 
-            // let derivation_path = acquire_derivation_path(matches)?;
-
             let word_count: usize = word_count.parse()?;
             let mnemonic_type = MnemonicType::for_word_count(word_count)?;
             let language = bip39::Language::English;
 
-            let (passphrase, passphrase_message) = if *use_mnemonic {
+            let (passphrase, passphrase_message) = if use_mnemonic {
                 // acquire_passphrase_and_message(matches)?
                 match prompt_passphrase(
                     "\nFor added security, enter a BIP39 passphrase\n\
@@ -215,15 +211,13 @@ fn main() -> Result<(), Box<dyn error::Error>> {
                     Err(_e) => ("".to_string(), "".to_string()),
                 }
             } else {
-                // no_passphrase_and_message()
                 ("".to_string(), "".to_string())
             };
-            // let no_outfile = matches.is_present(NO_OUTFILE_ARG.name);
 
             let skip_len_44_pubkeys = grind_matches
                 .iter()
                 .map(|g| {
-                    let target_key = if *ignore_case {
+                    let target_key = if ignore_case {
                         g.starts.to_ascii_uppercase()
                     } else {
                         g.starts.clone()
@@ -264,7 +258,7 @@ fn main() -> Result<(), Box<dyn error::Error>> {
                              found.load(Ordering::Relaxed)
                          );
                      }
-                     let (keypair, phrase) = if *use_mnemonic {
+                     let (keypair, phrase) = if use_mnemonic {
                          let mnemonic = Mnemonic::new(mnemonic_type, language);
                          let seed = Seed::new(&mnemonic, &passphrase);
                          let keypair = match derivation_path {
@@ -285,7 +279,7 @@ fn main() -> Result<(), Box<dyn error::Error>> {
                          continue;
                      }
                      let mut pubkey = bs58::encode(keypair.pubkey()).into_string();
-                     if *ignore_case {
+                     if ignore_case {
                          pubkey = pubkey.to_lowercase();
                      }
                      let mut total_matches_found = 0;
@@ -319,7 +313,7 @@ fn main() -> Result<(), Box<dyn error::Error>> {
                                      &format!("{}.json", keypair.pubkey())
                                  );
                              }
-                             if *use_mnemonic {
+                             if use_mnemonic {
                                  let divider = String::from_utf8(vec![b'='; phrase.len()]).unwrap();
                                  println!("{}\nFound matching key {}", &divider, keypair.pubkey());
                                  println!("\nSave this seed phrase{} to recover your new keypair:\n{}\n{}", passphrase_message, phrase, &divider);
@@ -344,7 +338,7 @@ fn main() -> Result<(), Box<dyn error::Error>> {
             skip_seed_phrase_validation,
         } => {
             let pubkey =
-                get_keypair_from_matches(*skip_seed_phrase_validation, keypair.clone(), config)?
+                get_keypair_from_matches(skip_seed_phrase_validation, keypair.clone(), config)?
                     .try_pubkey()?;
 
             if let Some(outfile) = outfile {
@@ -366,7 +360,7 @@ fn main() -> Result<(), Box<dyn error::Error>> {
             skip_seed_phrase_validation,
         } => {
             let keypair =
-                get_keypair_from_matches(*skip_seed_phrase_validation, keypair.clone(), config)?;
+                get_keypair_from_matches(skip_seed_phrase_validation, keypair.clone(), config)?;
             let simple_message = Message::new(
                 &[Instruction::new_with_bincode(
                     Pubkey::default(),
@@ -397,7 +391,7 @@ fn main() -> Result<(), Box<dyn error::Error>> {
                 outfile
             } else {
                 path.extend([".config", "solana", "id.json"]);
-                &path
+                path
             };
 
             if outfile.to_str().unwrap() != STDOUT_OUTFILE_TOKEN {
@@ -412,29 +406,17 @@ fn main() -> Result<(), Box<dyn error::Error>> {
 
             let keypair_name = "recover";
             let keypair = if let Some(path) = prompt_signer {
-                keypair_from_path(*skip_seed_phrase_validation, path, keypair_name, true)?
+                keypair_from_path(skip_seed_phrase_validation, &path, keypair_name, true)?
             } else {
-                let skip_validation = *skip_seed_phrase_validation;
+                let skip_validation = skip_seed_phrase_validation;
                 keypair_from_seed_phrase(keypair_name, skip_validation, true, None, true)?
             };
             output_keypair(&keypair, outfile.to_str().unwrap(), "recovered")?
         }
     }
 
-    // let matches = app(&default_num_threads, solana_version::version!())
-    //     .try_get_matches()
-    //     .unwrap_or_else(|e| e.exit());
-    // do_main(&matches).map_err(|err| DisplayError::new_as_boxed(err).into())
     Ok(())
 }
-
-// fn do_main(matches: &ArgMatches) -> Result<(), Box<dyn error::Error>> {
-//     match subcommand {
-//         ("grind", matches) => {
-//     }
-//
-//     Ok(())
-// }
 
 mod smallest_length_44_public_key {
     use solana_sdk::{pubkey, pubkey::Pubkey};
@@ -479,7 +461,6 @@ fn signer_from_path(
     path: &str,
     keypair_name: &str,
 ) -> Result<Box<dyn Signer>, Box<dyn error::Error>> {
-    let config = SignerFromPathConfig::default();
     signer_from_path_with_config(skip_seed_phrase_validation, path, keypair_name)
 }
 
@@ -583,204 +564,3 @@ fn grind_parse_args(
     grind_matches
 }
 
-// fn app<'a>(num_threads: &'a str, crate_version: &'a str) -> Command<'a> {
-//     Command::new(crate_name!())
-//          .about(crate_description!())
-//          .version(crate_version)
-//          .subcommand_required(true)
-//          .arg_required_else_help(true)
-//          .arg({
-//              let arg = Arg::new("config_file")
-//                  .short('C')
-//                  .long("config")
-//                  .value_name("FILEPATH")
-//                  .takes_value(true)
-//                  .global(true)
-//                  .help("Configuration file to use");
-//
-//              if let Some(config_file) = solana_cli_config::CONFIG_FILE.as_ref() {
-//                  arg.default_value(&config_file)
-//              }  else {
-//                  arg
-//              }
-//
-//          })
-//          .subcommand(
-//              Command::new("verify")
-//                  .about("Verify a keypair can sign and verify a message.")
-//                  .arg(
-//                      Arg::new("pubkey")
-//                          .index(1)
-//                          .value_name("PUBKEY")
-//                          .takes_value(true)
-//                          .required(true)
-//                          .help("Public key"),
-//                  )
-//                  .arg(
-//                      Arg::new("keypair")
-//                          .index(2)
-//                          .value_name("KEYPAIR")
-//                          .takes_value(true)
-//                          .help("Filepath or URL to a keypair"),
-//                  )
-//          )
-//          .subcommand(
-//              Command::new("new")
-//                  .about("Generate new keypair file from a random seed phrase and optional BIP39 passphrase")
-//                  .disable_version_flag(true)
-//                  .arg(
-//                      Arg::new("outfile")
-//                          .short('o')
-//                          .long("outfile")
-//                          .value_name("FILEPATH")
-//                          .takes_value(true)
-//                          .help("Path to generated file"),
-//                  )
-//                  .arg(
-//                      Arg::new("force")
-//                          .short('f')
-//                          .long("force")
-//                          .help("Overwrite the output file if it exists"),
-//                  )
-//                  .arg(
-//                      Arg::new("silent")
-//                          .short('s')
-//                          .long("silent")
-//                          .help("Do not display seed phrase. Useful when piping output to other programs that prompt for user input, like gpg")
-//                  )
-//                  .arg(
-//                      derivation_path_arg()
-//                  )
-//                  .key_generation_common_args()
-//                  .arg(no_outfile_arg().conflicts_with_all(&["outfile", "silent"]))
-//          )
-//          .subcommand(
-//              Command::new("grind")
-//                  .about("Grind for vanity keypairs")
-//                  .disable_version_flag(true)
-//                  .arg(
-//                      Arg::new("ignore_case")
-//                          .long("ignore-case")
-//                          .help("Performs case insensitive matches"),
-//                  )
-//                  .arg(
-//                      Arg::new("starts_with")
-//                          .long("starts-with")
-//                          .value_name("PREFIX:COUNT")
-//                          .number_of_values(1)
-//                          .takes_value(true)
-//                          .multiple_occurrences(true)
-//                          .multiple_values(true)
-//                          .validator(grind_validator_starts_with)
-//                          .help("Saves specified number of keypairs whos public key starts with the indicated prefix\nExample: --starts-with sol:4\nPREFIX type is Base58\nCOUNT type is u64")
-//                  )
-//                  .arg(
-//                      Arg::new("ends_with")
-//                          .long("ends-with")
-//                          .value_name("SUFFIX:COUNT")
-//                          .number_of_values(1)
-//                          .takes_value(true)
-//                          .multiple_occurrences(true)
-//                          .multiple_values(true)
-//                          .validator(grind_validator_ends_with)
-//                          .help("Saves specified number of keypairs whos public key ends with the indicated suffix\nExample: --ends-with ana:4\nSUFFIX type is Base58\nCOUNT type is u64")
-//                  )
-//                  .arg(
-//                      Arg::new("starts_and_ends_with")
-//                          .long("starts-and-ends-with")
-//                          .value_name("PREFIX:SUFFIX:COUNT")
-//                          .number_of_values(1)
-//                          .takes_value(true)
-//                          .multiple_occurrences(true)
-//                          .multiple_values(true)
-//                          .validator(grind_validator_starts_and_end_with)
-//                          .help("Saves specified number of keypairs whos public key starts and ends with the indicated perfix and suffix\nExample: --starts-and-ends-with sol:ana:4\nPREFIX and SUFFIX type is Base58\nCOUNT type is u64")
-//                  )
-//                  .arg(
-//                      Arg::new("num_threads")
-//                          .long("num-threads")
-//                          .value_name("NUMBER")
-//                          .takes_value(true)
-//                          .value_parser(value_parser!(usize))
-//                          .default_value(num_threads)
-//                          .help("Specify the number of grind threds")
-//                  )
-//                  .arg(
-//                      Arg::new("use_mnemonic")
-//                          .long("use-mnemonic")
-//                          .help("Generate usign a mnemonic key phrase. Expect a siginificant slowdown in this mode")
-//                  )
-//                  .arg(
-//                      derivation_path_arg()
-//                          .requires("use_mnemonic")
-//                  )
-//                  .key_generation_common_args()
-//                  .arg(
-//                      no_outfile_arg()
-//                          .requires("use_mnemonic")
-//                  )
-//          )
-//          .subcommand(
-//              Command::new("pubkey")
-//                  .about("Display the pubkey from a keypair file")
-//                  .disable_version_flag(true)
-//                  .arg(
-//                      Arg::new("keypair")
-//                          .index(1)
-//                          .value_name("KEYPAIR")
-//                          .takes_value(true)
-//                          .help("Filepath or URL to a keypair")
-//                  )
-//                  .arg(
-//                      Arg::new(SKIP_SEED_PHRASE_VALIDATION_ARG.name)
-//                          .long(SKIP_SEED_PHRASE_VALIDATION_ARG.long)
-//                          .help(SKIP_SEED_PHRASE_VALIDATION_ARG.help)
-//                  )
-//                  .arg(
-//                      Arg::new("outfile")
-//                          .short('o')
-//                          .long("outfile")
-//                          .value_name("FILEPATH")
-//                          .takes_value(true)
-//                          .help("Path to generated file")
-//                  )
-//                  .arg(
-//                      Arg::new("force")
-//                          .short('f')
-//                          .long("force")
-//                          .help("Overwrite the output file if it exists")
-//                  )
-//          )
-//          .subcommand(
-//              Command::new("recover")
-//             .about("Recover keypair from seed phrase and optional BIP39 passphrase")
-//                  .disable_version_flag(true)
-//                  .arg(
-//                      Arg::new("prompt_signer")
-//                          .index(1)
-//                          .value_name("KEYPAIR")
-//                          .takes_value(true)
-//                          .validator(is_prompt_signer_source)
-//                          .help("`prompt:` URI schme or `ASK` keyword")
-//                  )
-//                  .arg(
-//                      Arg::new("outfile")
-//                          .short('o')
-//                          .long("outfile")
-//                          .value_name("FILEPATH")
-//                          .takes_value(true)
-//                          .help("Path to generated file")
-//                  )
-//                  .arg(
-//                      Arg::new("force")
-//                          .short('f')
-//                          .long("force")
-//                          .help("Overwrite the output file if it exists")
-//                  )
-//                  .arg(
-//                      Arg::new(SKIP_SEED_PHRASE_VALIDATION_ARG.name)
-//                          .long(SKIP_SEED_PHRASE_VALIDATION_ARG.long)
-//                          .help(SKIP_SEED_PHRASE_VALIDATION_ARG.help)
-//                  )
-//          )
-// }
